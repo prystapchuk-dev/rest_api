ea3336e2e71d75beaba3707f9563ca42c5aa0448
siroga33 <zakaz_gzpt@ukr.net>
2021-06-05 18:12:46 +0300

    add methods read/create

--- product/read.php
+++ product/read.php
@@ -11,3 +11,37 @@ $database = new Database();
 $db = $database->getConnection();
 
 $product = new Product($db);
+
+$stmt = $product->read();
+$num = $product->rowCount();
+
+if ($num > 0) {
+    $products_arr = array();
+    $products_arr['records'] = array();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+        extract($row);
+
+        $product_item = array(
+            "id" => $id,
+            "name" => $name,
+            "description" => html_entity_decode($desciption),
+            "price" => $desciption,
+            "category_id" => $category_id,
+            "category_name" => $category_name
+        );
+
+        array_push($products_arr["records"], $product_item);
+    }
+
+    http_response_code(200);
+
+    echo json_encode($products_arr);
+
+} else {
+
+    http_response_code(404);
+
+    echo json_encode(array("message" => "Товары не найдены."), JSON_UNESCAPED_UNICODE);
+}
