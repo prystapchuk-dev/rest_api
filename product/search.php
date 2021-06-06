f2c51e5188c507ee606c89dfaf4603472dfc138b
siroga33 <zakaz_gzpt@ukr.net>
2021-06-06 14:13:50 +0300

    add methods search

---
+++ product/search.php
@@ -0,0 +1,51 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../config/core.php';
+include_once '../objects/product.php';
+include_once '../objects/product.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$product = new Product($db);
+
+$keywords = isset($_GET['s']) ? $_GET['s'] : "";
+
+$stmt = $product->search($keywords);
+$num = $product->rowCount();
+
+if ($num > 0) {
+    $product_arr = array();
+    $product_arr['records'] = array();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        $product_item = array(
+            "id" => $id,
+            "name" => $name,
+            "description" => html_entity_decode($desciption),
+            "price" => $price,
+            "category_id" => $category_id,
+            "category_name" => $category_name
+        );
+
+        array_push($product_arr['records'], $product_item);
+
+    }
+
+    http_response_code(200);
+
+    echo json_encode($products_arr);
+
+} else {
+
+    http_response_code(404);
+
+    echo json_encode(array("message" => "Товары не найдены."), JSON_UNESCAPED_UNICODE);
+
+}
+
