415ffede76d8440a2f1544d800daf60a164f592c
siroga33 <zakaz_gzpt@ukr.net>
2021-06-06 14:33:07 +0300

    create files read_paging and core.php

--- product/read_paging.php
+++ product/read_paging.php
@@ -4,22 +4,25 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 include_once '../config/core.php';
-include_once '../config/database.php';
+include_once '../shared/utilites.php';
+include_once '../config/database.phpp';
 include_once '../objects/product.php';
+
+$utiltites = new Utilites();
 
 $database = new Database();
 $db = $database->getConnection();
 
 $product = new Product($db);
 
-$keywords = isset($_GET['s']) ? $_GET['s'] : "";
-
-$stmt = $product->search($keywords);
-$num = $product->rowCount();
+$stmt = $product->readPaging($from_record_num, $records_per_page);
+$num = $stmt->rowCount();
 
 if ($num > 0) {
+
     $product_arr = array();
     $product_arr['records'] = array();
+    $product_arr['paging'] = array();
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
@@ -37,15 +40,18 @@ if ($num > 0) {
 
     }
 
+    $total_rows = $product->count();
+    $page_url = "{$home_url}product/read_paging.php";
+    $paging = $utiltites->getPaging($page, $total_rows, $records_per_page, $page_url);
+    $product_arr['paging'] = $paging;
+
     http_response_code(200);
 
-    echo json_encode($products_arr);
+    echo json_encode($product_arr);
 
 } else {
 
     http_response_code(404);
 
     echo json_encode(array("message" => "Товары не найдены."), JSON_UNESCAPED_UNICODE);
-
 }
-
